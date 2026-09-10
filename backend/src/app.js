b527db85947694257c0d8dc5470add4ef1dfafe2
Dinesh <dineshbjadhav2003@gmail.com>
2026-09-10 18:10:42 +0530

    Fix production CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,12 @@ import Razorpay from "razorpay"
 
 const app = express();
 
-app.use(cors())
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
